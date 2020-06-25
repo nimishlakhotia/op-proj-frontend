39200e3f99596bb5fb8403ae6712097385b59f50
dropTableUsers42 <rwitaban0001@gmail.com>
2020-06-25 13:24:19 +0530

    Added Homepage

---
+++ src/app/app-ui.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+
+const uiModules = [
+  MatSidenavModule,
+  MatIconModule,
+  MatButtonModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: uiModules,
+  exports: uiModules
+})
+export class AppUiModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
-  title = 'op-app';
+export class AppComponent implements OnInit, OnDestroy{
+  title = 'THE OPPORTUNITY PROJECT.';
+  logo = 'LOGO';
+  wishlist = 'WISHLIST'
+
+  public isMenuOpen: boolean = false;
+
+  bodyTag: HTMLBodyElement = document.getElementsByTagName('body')[0];
+  htmlTag: HTMLElement = document.getElementsByTagName('html')[0];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    //this.bodyTag.classList.add('home-container');
+    //this.htmlTag.classList.add('home-container');
+  }
+  
+  ngOnDestroy() {
+    //this.bodyTag.classList.remove('home-container');
+    //this.htmlTag.classList.remove('home-container');
+  }
 }
