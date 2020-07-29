ddd577167c0e520ed700d758336ca8e324a99b5b
dropTableUsers42 <rwitaban0001@gmail.com>
2020-07-30 01:49:37 +0530

    Made header responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,51 @@
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./header.component.css',
+  './header-colors.component.css'],
 })
 export class HeaderComponent implements OnInit {
 
   logo = 'LOGO';
-  wishlist = 'WISHLIST'
-  public isMenuOpen: boolean = false;
+  wishlist = 'WISHLIST';
 
-  constructor() { }
+  @Input('page') page_style;
+
+  mat_style = 'default';
+  
+  logo_class = {
+    'logo': true,
+    'mat-core': this.page_style == 'core',
+    'mat-default': this.page_style != 'core',
+  }
+
+  wishlist_class = {
+    'wishlist': true,
+    'mat-core': this.page_style == 'core',
+    'mat-default': this.page_style != 'core',
+  }
+
+
+  @Output() onSidenavToggle: EventEmitter<any> = new EventEmitter<any>();
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  sidenavToggle() {
+    this.onSidenavToggle.emit();
+  }
+
+  showHeader() {
+    if(this.router.url.includes('login') || this.router.url.includes('register'))
+    {
+      return false;
+    }
+    return true;
   }
 
 }
